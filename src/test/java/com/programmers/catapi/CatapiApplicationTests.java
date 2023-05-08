44329958f1fc2af94e253af5d8d81bb02be867ae
leegwichan <cnddkscndgus@naver.com>
2023-05-08 09:29:35 +0900

    init (Spring) : 프로젝트 구성
    - java 17, Spring 3.0.6

---
+++ src/test/java/com/programmers/catapi/CatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.catapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
